06d62134f4f5f426588de32b547980b34397d152
Pavlos Vinieratos <pvinis@gmail.com>
2018-03-11 12:57:11 +0100

    more

--- lambda/src/constants.js
+++ lambda/src/constants.js
@@ -3,7 +3,7 @@
 module.exports = Object.freeze({
     
     //App-ID. TODO: set to your own Skill App ID from the developer portal.
-    //appId : 'amzn1.ask.skill.123',
+    appId : 'amzn1.ask.skill.18293fb4-8868-4a2a-8826-4119922d33ec',
 
     // when true, the skill logs additional detail, including the full request received from Alexa
     debug : true
